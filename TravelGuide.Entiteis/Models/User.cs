48f92011ee5b10545c5aa651f7e373690743c97c
Abdelrahman Sameer <154366196+Abdelrahman232323@users.noreply.github.com>
2024-09-06 22:07:04 +0300

    Add files via upload

---
+++ TravelGuide.Entiteis/Models/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelGuide.Entiteis.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId;
+
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [MaxLength(50)]
+        public string Address { get; set; }
+
+        [MaxLength(255)]
+        public string Email { get; set; }
+        [MaxLength(255)]
+        public string Passsword { get; set; }
+        [MaxLength(50)]
+        public string Nationality { get; set; }
+        [MaxLength(50)]
+        public string HotelName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DOB { get; set; }
+
+        [MaxLength(10)]
+        public string Gender { get; set; }
+
+        [MaxLength(20)]
+        public string UserType { get; set; }
+
+        [MaxLength(255)]
+        public string Token { get; set; }
+
+
+        public ICollection<WatchlistItem> WatchlistItems { get; set; }
+
+
+    }
+}
---
+++ TravelGuide.Entiteis/Models/WatchlistItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelGuide.Entiteis.Models
+{
+    public class WatchlistItem
+    {
+        [Key]
+        public int WatchlistItemId { get; set; }
+
+       // [ForeignKey("User")]
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+
+        public int ItemID { get; set; }
+
+        [MaxLength(20)]
+        public string ItemType { get; set; }
+
+        public User User { get; set; }
+
+    }
+}
