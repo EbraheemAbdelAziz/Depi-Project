fdd4b8e6c50712b87eaed459d81c1e941062b98e
Nada125 <112761757+Nada125@users.noreply.github.com>
2024-09-06 18:52:43 +0300

    Add files via upload

---
+++ TravelGuide.Entiteis/Models/Hotel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectModels.Models
+{
+    public class Hotel
+    {
+        [Key]
+        public int HotelId { get; set; }
+        [MaxLength(50)]
+        public string HotelName { get; set; }
+        [MaxLength(50)]
+        public string City { get; set; }
+        [MaxLength(100)]
+        public string Address { get; set; }
+        [MaxLength(50)]
+        public string PhoneNumber { get; set; }
+        [MaxLength(50)]
+        public string Email { get; set; }
+        public int Rating { get; set; }     // Rate from 1 to 5
+
+
+       
+
+    }
+}
