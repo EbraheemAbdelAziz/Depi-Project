600f32e5842858bc9bd8cd6392b46bc15db5c931
Mohamed <Mohamed@DESKTOP-CTSCH17>
2024-09-06 21:53:20 +0300

    Create 2 table TravelPackage and PackageBooking

---
+++ TravelGuide.Entiteis/Models/PackageBooking.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelGuide.Entiteis.Models
+{
+    public class PackageBooking
+    {
+        [Key]
+        public int BookingId { get; set; }
+
+        [Required]
+        public int PackageId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public DateTime BookingDate { get; set; }
+
+        [Required]
+        public int NumberOfGuests { get; set; }
+
+        [Required]
+        public decimal TotalPrice { get; set; }
+
+        // Foreign key relationships
+        [ForeignKey("PackageId")]
+        public virtual TravelPackages TravelPackage { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+    }
+}
---
+++ TravelGuide.Entiteis/Models/TravelPackages.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelGuide.Entiteis.Models
+{
+    public class TravelPackages
+    {
+        [Key]
+        public int PackageId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string PackageName { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Destination { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Duration { get; set; }  // Duration in days
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public int MaxGuests { get; set; }
+
+        public decimal? Rating { get; set; }
+
+        [MaxLength(255)]
+        public string ImageURL { get; set; }
+
+        // Navigation property
+        public virtual ICollection<PackageBooking> PackageBookings { get; set; }
+    }
+}
