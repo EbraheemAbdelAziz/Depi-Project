1253cf67bd9b6528c6aa557bddb63de4e2da5b93
Nada125 <112761757+Nada125@users.noreply.github.com>
2024-09-06 19:00:35 +0300

    Update Room.cs

--- TravelGuide.Entiteis/Models/Room.cs
+++ TravelGuide.Entiteis/Models/Room.cs
@@ -1,23 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace ProjectModels.Models
+namespace TravelGuide.Entiteis.Models
 {
     public class Room
     {
         public int RoomId { get; set; }
-        public int RoomNumber { get; set; }
-        public string RoomType { get; set; }
-        public int Capacity { get; set; }
-        public decimal PricePerNight { get; set; }
-        public bool Availability { get; set; }
 
+        [ForeignKey(nameof(hotel))]
         public int hotelId { get; set; }
         public Hotel hotel { get; set; } 
+        
+        public int RoomNumber { get; set; }
+
+        [MaxLength(50)]
+        public string RoomType { get; set; }
+        
+        public int Capacity { get; set; }
+        
+        public decimal PricePerNight { get; set; }
+        
+        public bool Availability { get; set; }
+
+        
         
     }
 }
