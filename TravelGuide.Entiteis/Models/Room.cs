01ca40c9d1cb135da6df7072b77a7e6d1baeb6ed
Nada125 <112761757+Nada125@users.noreply.github.com>
2024-09-06 18:53:17 +0300

    Add files via upload

---
+++ TravelGuide.Entiteis/Models/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectModels.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public int RoomNumber { get; set; }
+        public string RoomType { get; set; }
+        public int Capacity { get; set; }
+        public decimal PricePerNight { get; set; }
+        public bool Availability { get; set; }
+
+        public int hotelId { get; set; }
+        public Hotel hotel { get; set; } 
+        
+    }
+}
